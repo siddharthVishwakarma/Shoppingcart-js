a7f3bee6e8a8c27492becf9ed72a4047dfa7de45
Siddharth Vishwakarma <sid.sv14@gmail.com>
2022-06-08 13:02:34 +0530

    ✅ local storage added

--- main.js
+++ main.js
@@ -5,38 +5,39 @@ let shopItemData = [
     id: 1,
     img: "images/img-1.jpg",
     name: "Casual Shirt",
-    desc: "Ad non enim Lorem esse labore ea dolore ipsum in consequat labore ad laborum exercitation.",
+    desc: "Ad non enim Lorem esse labore ea dolore ipsum in consequat labore ad.",
     price: 100,
   },
   {
     id: 2,
     img: "images/img-2.jpg",
     name: "Formal Shirt",
-    desc: "Ad non enim Lorem esse labore ea dolore ipsum in consequat labore ad laborum exercitation.",
+    desc: "Ad non enim Lorem esse labore ea dolore ipsum in consequat labore ad.",
     price: 145,
   },
   {
     id: 3,
     img: "images/img-8.png",
     name: "Tshirt",
-    desc: "Ad non enim Lorem esse labore ea dolore ipsum in consequat labore ad laborum exercitation.",
+    desc: "Ad non enim Lorem esse labore ea dolore ipsum in consequat labore ad.",
     price: 45,
   },
   {
     id: 4,
     img: "images/img-4.jpg",
     name: "Mens Suit",
-    desc: "Ad non enim Lorem esse labore ea dolore ipsum in consequat labore ad laborum exercitation.",
+    desc: "Ad non enim Lorem esse labore ea dolore ipsum in consequat labore ad.",
     price: 455,
   },
 ];
 
-let basket = [];
+let basket = JSON.parse(localStorage.getItem("data")) || []; //retriving the data from the local storage
 
 let genreateShop = () => {
   return (shop.innerHTML = shopItemData
     .map((x) => {
       let { id, img, desc, name, price } = x;
+      let search = basket.find((x) => x.id === id) || []; //check for the item id in the basket
       return `
     <div id=product-id-${id} class="item">
     <img width="220" src=${img} alt="image">
@@ -47,7 +48,9 @@ let genreateShop = () => {
             <h2>$ ${price}</h2>
             <div class="buttons">
                 <i onclick="decriment(${id})" class="bi bi-dash-square"></i>
-                <div id=${id} class="quantity">0</div>
+                <div id=${id} class="quantity">
+                ${search.item === undefined ? 0 : search.item}
+                </div>
                 <i onclick="increment(${id})" class="bi bi-plus-square"></i>
             </div>
         </div>
@@ -75,6 +78,7 @@ let increment = (id) => {
   } else {
     search.item += 1;
   }
+  localStorage.setItem("data", JSON.stringify(basket)); //adding local storage to save incriment data
   //   console.log(basket);
   update(selectedItem);
 };
@@ -86,13 +90,14 @@ let decriment = (id) => {
 
   if (search.item === 0) return; // If their is 0 item in basket then stop
   else {
-    search.item -= 1;
+    search.item -= 1; //else remove item one by one
   }
+  localStorage.setItem("data", JSON.stringify(basket)); //adding local storage to save decriment data
   //   console.log(basket);
   update(selectedItem);
 };
 
-// Update the quantity of item inthe card
+// Update the quantity of item in the card
 let update = (id) => {
   let search = basket.find((x) => x.id === id);
   //   console.log(search.item);
@@ -108,3 +113,5 @@ let calculate = () => {
   let total = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
   cartIcon.innerHTML = total;
 };
+
+calculate(); //For saving the data in the cart
